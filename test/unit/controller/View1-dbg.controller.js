5253a09b219737e693f2e51eb1f76b1807e4048e
S0021770343 <debabrata.bhattacharjee@wipro.com>
2020-07-23 12:52:57 +0000

    Import content for version 1.0.21

---
+++ test/unit/controller/View1-dbg.controller.js
@@ -0,0 +1,16 @@
+/*global QUnit*/
+
+sap.ui.define([
+	"YY1_MAT_CONSUME_UI5/Material_Consume_13/controller/View1.controller"
+], function (Controller) {
+	"use strict";
+
+	QUnit.module("View1 Controller");
+
+	QUnit.test("I should test the View1 controller", function (assert) {
+		var oAppController = new Controller();
+		oAppController.onInit();
+		assert.ok(oAppController);
+	});
+
+});
